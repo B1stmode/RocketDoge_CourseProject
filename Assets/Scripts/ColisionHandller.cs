6f0d03616fa785a1a1a0baa45bc151aa067b5f85
B1stmode <tima.gamedev@gmail.com>
2025-04-19 11:38:20 +0200

    Added 3 new scenes

---
+++ Assets/Scripts/ColisionHandller.cs
@@ -0,0 +1,73 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ColisionHandller : MonoBehaviour
+{
+    [SerializeField] float levelLoadDelay = 2f;
+    [SerializeField] AudioClip success;
+    [SerializeField] AudioClip crash;
+    bool isControllable = true;
+
+    AudioSource audioSource;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (isControllable == false)
+        {
+            return;
+        }
+
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("This is a friendly object");
+                break;
+            case "Finish":
+                StartSuccessSequence();
+                break; 
+            default: 
+                StartCrashSequence();
+                break;
+        }
+    }
+
+    void StartSuccessSequence()
+    {
+        isControllable = false;
+        audioSource.Stop();
+        audioSource.PlayOneShot(success);
+        GetComponent<Movement>().enabled = false;
+        Invoke ("LoadNextLevel", levelLoadDelay);
+    }
+
+    void StartCrashSequence()
+    {
+        isControllable = false;
+        audioSource.PlayOneShot(crash);
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", levelLoadDelay);
+
+
+    }
+
+    void ReloadLevel()  
+        {
+            int currentScene = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(currentScene);
+        }
+
+        void LoadNextLevel()
+        {
+            int currentScene = SceneManager.GetActiveScene().buildIndex;
+            int nextScene = currentScene + 1;
+            if (nextScene == SceneManager.sceneCountInBuildSettings)
+            {
+                nextScene = 0;
+            }
+            SceneManager.LoadScene(nextScene);
+        }
+}
